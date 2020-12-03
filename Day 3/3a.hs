68bc50736243932338ced7f212884019012376cb
Shea Cardozo <sacardoz@uwaterloo.ca>
2020-12-03 16:25:24 -0500

    day 3

---
+++ Day 3/3a.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Lib ()
+
+filename :: FilePath
+filename = "Day 3/3.txt"
+
+main :: IO ()
+main = do
+    contents <- readFile filename
+    print . result . lines $ contents
+
+result ::  [String] -> Int
+result =  ski 1 3
+
+ski :: Int -> Int -> [String] -> Int
+ski i j hill
+    | i >= length hill = 0
+    | (hill !! i) !! (j `mod` (length . head $ hill)) == '#' = 1 + ski (i+1) (j+3) hill
+    | otherwise = ski (i+1) (j+3) hill
---
+++ Day 3/3b.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Lib ()
+
+filename :: FilePath
+filename = "Day 3/3.txt"
+
+main :: IO ()
+main = do
+    contents <- readFile filename
+    print . result . lines $ contents
+
+result ::  [String] -> Int
+result content =  ski 1 1 content * ski 1 3 content * ski 1 5 content * ski 1 7 content * ski 2 1 content
+
+ski :: Int -> Int -> [String] -> Int
+ski d p s = do
+        let dp = \i j hill -> case () of 
+              _ | i >= length hill -> 0
+                | (hill !! i) !! (j `mod` (length . head $ hill)) == '#' -> 1 + dp (i+d) (j+p) hill
+                | otherwise -> dp (i+d) (j+p) hill
+
+        dp d p s
+        
