6bf952114af7593f3da9c62051dbc0e68001df7e
Shea Cardozo <sacardoz@uwaterloo.ca>
2020-12-09 13:53:49 -0500

    day 5

---
+++ Solutions/Day 5/5b.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import Lib ()
+
+filename :: FilePath
+filename = "Solutions/Day 5/5.txt"
+
+main :: IO ()
+main = do
+    contents <- readFile filename
+    print . result . lines $ contents
+
+
+result ::  [String] -> Int
+result content = do
+    let ids = map seatID content
+    let fids = [minimum ids..maximum ids]
+    head (filter (`notElem` ids) fids)
+
+
+seatID :: String -> Int
+seatID pass = rowNum (take 7 pass) * 8 + colNum (reverse . take 3 .reverse $ pass)
+
+
+rowNum :: String -> Int
+rowNum pass = head (foldl rowHalf [0..127] pass)
+
+rowHalf :: [Int] -> Char -> [Int]
+rowHalf seats i
+    | i == 'F' = take (length seats `div` 2) seats
+    | otherwise = reverse . take (length seats `div` 2) . reverse $ seats
+
+colNum :: String -> Int 
+colNum pass = head (foldl colHalf [0..7] pass)
+
+colHalf :: [Int] -> Char -> [Int]
+colHalf seats i
+    | i == 'L' = take (length seats `div` 2) seats
+    | otherwise = reverse . take (length seats `div` 2) . reverse $ seats
+
+
